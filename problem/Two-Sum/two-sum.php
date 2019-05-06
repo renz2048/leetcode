03dc33cdb79cbc877c8980a9902f2792fca5dca1
renzheng <renz@koal.com>
2019-05-07 00:34:23 +0800

    Two Sum with PHP

---
+++ problem/Two-Sum/two-sum.php
@@ -0,0 +1,18 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        for ($i = 0; $i < count($nums); $i++) {
+            for ($j = $i + 1; $j < count($nums); $j++) {
+                if ($nums[$i] + $nums[$j] == $target) {
+                    return array($i, $j);
+                }
+            }
+         }
+        throw new ErrorException("No two sum solution");
+    }
+}
